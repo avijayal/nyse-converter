355e916e00f8843a13bd7e11b65903a3e5ae2901
vijayalakshmi <3002.vijayalakshmi@gmail.com>
2023-07-30 18:09:55 +0530

    core logic to convert csv to json

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-def sum_n(n):
-    if type(n)!=int or n<=0:
-        raise ValueError('Invalid Type or Value')
-    res=(n*(n+1)/2)
-    print(res)
-    return 
+# def sum_n(n):
+#     if type(n)!=int or n<=0:
+#         raise ValueError('Invalid Type or Value')
+#     res=(n*(n+1)/2)
+#     print(res)
+#     return 
+
+from dask import dataframe as dd
+def main():
+    print('File format conversion started')
+    df=dd.read_csv(
+        'data/nyse_all/nyse_data/NYSE*.txt.gz',
+        names=['ticker', 'trade_date', 'open_price', 'low_price',
+                'high_price', 'close_price', 'volume'],
+        blocksize=None
+    )
+    print('DataFrame is created and will be written in json format')
+    df.to_json(
+    'data/nyse_all/nyse_json/part-*.json.gz',
+    orient='records',
+    lines=True,
+    compression='gzip',
+    name_function=lambda i:'%05d' %i
+    )
+    print('File format conversion completed')
 
 if __name__=='__main__':
-    sum_n(10)
+    main()
