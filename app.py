ee0e0b0b8d7d82693aa057baaf2f500f33b94b02
vijayalakshmi <3002.vijayalakshmi@gmail.com>
2023-07-31 17:16:36 +0530

    refractes code using environment variable

--- app.py
+++ app.py
@@ -5,18 +5,26 @@
 #     print(res)
 #     return 
 
+# make changes to nyse_converter to use environment variables
+#   SRC_DIR:'data/nyse_all/nyse_data'
+#   TGT_DIR:'data/nyse_all/nyse_json'
+# make sure to run and validate the code after making changes using environment variables
+# make sure to commit and push changes to remote git repository
+import os
 from dask import dataframe as dd
 def main():
+    src_dir = os.environ['SRC_DIR']
+    tgt_dir = os.environ['TGT_DIR']
     print('File format conversion started')
     df=dd.read_csv(
-        'data/nyse_all/nyse_data/NYSE*.txt.gz',
+        f'{src_dir}/NYSE*.txt.gz',
         names=['ticker', 'trade_date', 'open_price', 'low_price',
                 'high_price', 'close_price', 'volume'],
         blocksize=None
     )
     print('DataFrame is created and will be written in json format')
     df.to_json(
-    'data/nyse_all/nyse_json/part-*.json.gz',
+    f'{tgt_dir}/part-*.json.gz',
     orient='records',
     lines=True,
     compression='gzip',
